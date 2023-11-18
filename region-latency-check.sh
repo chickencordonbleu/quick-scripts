62b9fc72f9f6e65617345be50323b193eedb7886
chickencordonbleu <43434177+chickencordonbleu@users.noreply.github.com>
2023-11-18 19:49:17 +0400

    sort by fastest in each continent

--- region-latency-check.sh
+++ region-latency-check.sh
@@ -8,11 +8,42 @@ check_latency() {
     local region=$1
     local endpoint="ec2.$region.amazonaws.com"
 
-    echo "Checking latency to $region ($endpoint)..."
-    ping -c 4 $endpoint | tail -1 | awk -F '/' '{print "Average Latency: " $5 " ms"}'
+    local latency
+    latency=$(ping -c 4 $endpoint | tail -1 | awk -F '/' '{print $5}')
+    
+    # Output region and latency
+    echo "$region $latency"
 }
 
-# Check latency for each AWS region
+# Function to categorize region by continent
+get_continent() {
+    case $1 in
+        eu-*) echo "Europe" ;;
+        us-*) echo "North America" ;;
+        ap-*) echo "Asia Pacific" ;;
+        sa-*) echo "South America" ;;
+        af-*) echo "Africa" ;;
+        me-*) echo "Middle East" ;;
+        *) echo "Other" ;;
+    esac
+}
+
+# Temporary file for results
+results_file=$(mktemp)
+
+# Check latency for each region in parallel
 for region in $regions; do
-    check_latency "$region"
+    (check_latency "$region" >> "$results_file") &
 done
+
+# Wait for all background processes to finish
+wait
+
+# Read results and sort
+while read -r region latency; do
+    continent=$(get_continent "$region")
+    printf "%-15s %-20s %s\n" "$continent" "$region" "$latency"
+done < "$results_file" | sort -k1,1 -k3,3n
+
+# Clean up
+rm "$results_file"
